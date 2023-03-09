11af0c3ed3b6c5d966e388a433b6f9e9cd9ac437
hikurt <32220244+hikurt@users.noreply.github.com>
2023-03-09 12:55:32 +0100

    Added styles for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todos from './components/Todos';
+import NewTodo from './components/NewTodo';
+import Todo from './models/todo';
 
 function App() {
+  const [ todos, setTodos ] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
-      <Todos items={['Learn React', 'Learn TypeScript']} />
+      <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todos} />
     </div>
   );
 }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+// import Todo from "../models/todo";
+import classes from './TodoItem.module.css';
+
+const TodoItem: React.FC<{text: string}> = (props) => {
+  return (
+    <li className={classes.item}>{props.text}</li> 
+  );
+}
+export default TodoItem;
