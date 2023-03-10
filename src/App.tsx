5d99df943bbf7ae47cddec08d5d164044e902c6b
hikurt <32220244+hikurt@users.noreply.github.com>
2023-03-10 10:08:17 +0100

    Added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Todos from './components/Todos';
 import NewTodo from './components/NewTodo';
-import Todo from './models/todo';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  const [ todos, setTodos ] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
 
   return (
     <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos} />
+      <TodosContextProvider>
+        <NewTodo />
+        <Todos />
+      </TodosContextProvider>
     </div>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,11 @@
 // import Todo from "../models/todo";
 import classes from './TodoItem.module.css';
 
-const TodoItem: React.FC<{text: string}> = (props) => {
+const TodoItem: React.FC<{text: string; onRemoveTodo: () => void}> = (props) => {
   return (
-    <li className={classes.item}>{props.text}</li> 
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li> 
   );
 }
 export default TodoItem;
